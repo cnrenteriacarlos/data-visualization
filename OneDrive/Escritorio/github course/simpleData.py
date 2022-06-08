fb07c750f69cca1dc9a6bf90903cbb4de12d33df
Carlos Renteria <crenteriacarlos@gmail.com>
2022-06-08 07:40:03 -0600

    add simpleData

---
+++ OneDrive/Escritorio/github course/simpleData.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+#Using list
+''''
+carLoans = [[1, 34689.96, 687.23, 202.93, 484.3, 34205.66, 60, 0.0702,'Toyota Sienna'],
+           [2, 34205.66, 687.23, 200.1, 487.13, 33718.53, 60, 0.0702,'Toyota Sienna'],
+           [3, 33718.53, 687.23, 197.25, 489.98, 33228.55, 60, 0.0702,'Toyota Sienna'],
+           [4, 33228.55, 687.23, 194.38, 492.85, 32735.7, 60, 0.0702,'Toyota Sienna'],
+           [5, 32735.7, 687.23, 191.5, 495.73, 32239.97, 60, 0.0702,'Toyota Sienna']]
+'''
+#Using numpy array
+
+'''carLoans =np.array( [[1, 34689.96, 687.23, 202.93, 484.3, 34205.66, 60, 0.0702,'Toyota Sienna'],
+           [2, 34205.66, 687.23, 200.1, 487.13, 33718.53, 60, 0.0702,'Toyota Sienna'],
+           [3, 33718.53, 687.23, 197.25, 489.98, 33228.55, 60, 0.0702,'Toyota Sienna'],
+           [4, 33228.55, 687.23, 194.38, 492.85, 32735.7, 60, 0.0702,'Toyota Sienna'],
+           [5, 32735.7, 687.23, 191.5, 495.73, 32239.97, 60, 0.0702,'Toyota Sienna']])'''
+
+#Using dictionaries.
+carLoans = {'Month': {0: 1, 1: 2, 2: 3, 3: 4, 4: 5},
+             'Starting Balance': {0: 34689.96,1: 34205.66,2: 33718.53,3: 33228.55,4: 32735.7},
+             'Repayment': {0: 687.23, 1: 687.23, 2: 687.23, 3: 687.23, 4: 687.23},
+             'Interest Paid': {0: 202.93, 1: 200.1, 2: 197.25, 3: 194.38, 4: 191.5},
+             'Principal Paid': {0: 484.3, 1: 487.13, 2: 489.98, 3: 492.85, 4: 495.73},
+             'New Balance': {0: 34205.66,1: 33718.53,2: 33228.55,3: 32735.7,4: 32239.97},
+             'term': {0: 60, 1: 60, 2: 60, 3: 60, 4: 60},
+             'interest_rate': {0: 0.0702, 1: 0.0702, 2: 0.0702, 3: 0.0702, 4: 0.0702},
+             'car_type': {0: 'Toyota Sienna',1: 'Toyota Sienna',2: 'Toyota Sienna',3: 'Toyota Sienna',4: 'Toyota Sienna'}}
+
+colNames = ['Month',
+            'Starting Balance',
+            'Repayment',
+            'Interest Paid',
+            'Principal Paid',
+            'New Balance',
+            'term',
+            'interest_rate',
+            'car_type']
+
+df = pd.DataFrame(data=carLoans, columns=colNames)
+
+print(df)
+#this is the end
+
+#help(pd.DataFrame) #request help for commands
